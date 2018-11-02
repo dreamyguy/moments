1d98258b11f101952f1851f1d13da630bb295462
Wallace Sidhrée <i@dreamyguy.com>
2018-11-03 00:38:24 +0100

    :umbrella: Refactor 'test' setup and make it support 'jest' and 'enzyme' outside CRA2's black box

---
+++ src/test/setupTests.js
@@ -0,0 +1,12 @@
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+// Mock localStorage
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+};
+global.localStorage = localStorageMock;
