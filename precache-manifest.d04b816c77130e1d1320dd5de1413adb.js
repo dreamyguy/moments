5a0a00d2a5aa354404e406993b32174d063d9743
Wallace Sidhrée <i@dreamyguy.com>
2019-05-26 14:12:58 +0200

    Updates

---
+++ precache-manifest.d04b816c77130e1d1320dd5de1413adb.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca1758306976fac394963a9eadc6905",
+    "url": "/moments/index.html"
+  },
+  {
+    "revision": "5e4703f4b1f3074c2dec",
+    "url": "/moments/static/css/2.99183fa4.chunk.css"
+  },
+  {
+    "revision": "caa67e0f775a1469bf3c",
+    "url": "/moments/static/css/main.ad147b7d.chunk.css"
+  },
+  {
+    "revision": "5e4703f4b1f3074c2dec",
+    "url": "/moments/static/js/2.441751c2.chunk.js"
+  },
+  {
+    "revision": "caa67e0f775a1469bf3c",
+    "url": "/moments/static/js/main.a48421d3.chunk.js"
+  },
+  {
+    "revision": "ccc33ad204c88f119456",
+    "url": "/moments/static/js/runtime~main.f5e79379.js"
+  },
+  {
+    "revision": "561246a3549d3b797b4b611652a79d7c",
+    "url": "/moments/static/media/Montserrat-Black.561246a3.otf"
+  },
+  {
+    "revision": "468fb6c77e29f39a59d2d00b3d99d5ec",
+    "url": "/moments/static/media/Montserrat-BlackItalic.468fb6c7.otf"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "/moments/static/media/Montserrat-Bold.9c71d42b.otf"
+  },
+  {
+    "revision": "1fb2347e5e70361a92e8d4c4281cf457",
+    "url": "/moments/static/media/Montserrat-BoldItalic.1fb2347e.otf"
+  },
+  {
+    "revision": "433ac418a603294fc5891dde6a73cd24",
+    "url": "/moments/static/media/Montserrat-ExtraBold.433ac418.otf"
+  },
+  {
+    "revision": "880b0f4de395298b9c1316476d406cae",
+    "url": "/moments/static/media/Montserrat-ExtraBoldItalic.880b0f4d.otf"
+  },
+  {
+    "revision": "2ec7cecb19c1b42ca9ca709971016156",
+    "url": "/moments/static/media/Montserrat-ExtraLight.2ec7cecb.otf"
+  },
+  {
+    "revision": "ca2d2548eadf6c546780557a20874f01",
+    "url": "/moments/static/media/Montserrat-ExtraLightItalic.ca2d2548.otf"
+  },
+  {
+    "revision": "078a83ae6f40dfe6dd70381d4c2b6e60",
+    "url": "/moments/static/media/Montserrat-Italic.078a83ae.otf"
+  },
+  {
+    "revision": "d4a8834fa8f57f0929b9f4ef89584361",
+    "url": "/moments/static/media/Montserrat-Light.d4a8834f.otf"
+  },
+  {
+    "revision": "64036f65a43dbe4ad8e81f2f237a4d99",
+    "url": "/moments/static/media/Montserrat-LightItalic.64036f65.otf"
+  },
+  {
+    "revision": "d815b0a29adf3450c55f56e2fb813be4",
+    "url": "/moments/static/media/Montserrat-Medium.d815b0a2.otf"
+  },
+  {
+    "revision": "96947c00edbd07b60480ede114296275",
+    "url": "/moments/static/media/Montserrat-MediumItalic.96947c00.otf"
+  },
+  {
+    "revision": "92db9a0772b3732e6d686fec3711af42",
+    "url": "/moments/static/media/Montserrat-Regular.92db9a07.otf"
+  },
+  {
+    "revision": "bb3740d350b0186ce32b5678972bf061",
+    "url": "/moments/static/media/Montserrat-SemiBold.bb3740d3.otf"
+  },
+  {
+    "revision": "977365caf72d2be914c1b88f903ad355",
+    "url": "/moments/static/media/Montserrat-SemiBoldItalic.977365ca.otf"
+  },
+  {
+    "revision": "cb2144b361907d6549a80a5203f07347",
+    "url": "/moments/static/media/Montserrat-Thin.cb2144b3.otf"
+  },
+  {
+    "revision": "0601e651a650facae740e7a707f31156",
+    "url": "/moments/static/media/Montserrat-ThinItalic.0601e651.otf"
+  },
+  {
+    "revision": "3758c64fcdbcac361329aa2c01068df4",
+    "url": "/moments/static/media/chevron-down.3758c64f.svg"
+  },
+  {
+    "revision": "ef32dba00933f3c84f5c2b49ab611573",
+    "url": "/moments/static/media/moments-logo-header.ef32dba0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/moments/precache-manifest.19f39a25606973404239f1973f40f371.js"
+  "/moments/precache-manifest.d04b816c77130e1d1320dd5de1413adb.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/moments/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moments/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
