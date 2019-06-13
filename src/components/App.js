2b339186dcfe2475f4ffebbd95d38767ffde1cbe
Wallace Sidhrée <i@dreamyguy.com>
2019-06-13 23:36:32 +0200

    :octopus: Introduce 'fork on github' badge and bump version

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 // Import components
 import Header from './layout/Header';
 import Footer from './layout/Footer';
+import GithubCorner from './layout/GithubCorner';
 import ModePicker from './../components/time/ModePicker';
 import NowTicker from './../components/time/NowTicker';
 
@@ -11,6 +12,7 @@ class App extends Component {
   render () {
     return (
       <div className="app">
+        <GithubCorner href="https://github.com/dreamyguy/moments"/>
         <Header/>
         <ModePicker/>
         <div className="content">
